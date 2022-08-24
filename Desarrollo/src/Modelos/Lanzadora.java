954f9da1482f46369836b50494d7a9e5114218b8
kalixo97 <kalix530@gmail.com>
2022-08-23 19:31:23 -0500

    Creacion de clases de java

---
+++ Desarrollo/src/Modelos/Lanzadora.java
@@ -0,0 +1,13 @@
+package Modelos;
+
+public class Lanzadora {
+
+    public static void main(String[] args) {
+
+        Empleado empleado1 = new Empleado();
+        Empresa empresa1 = new Empresa();
+        MovimientoDinero transaccion = new MovimientoDinero();
+
+
+    }
+}
