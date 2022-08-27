1c6e4e7aea3fdb3ad83210281d016238094ed9c6
kalixo97 <kalix530@gmail.com>
2022-08-26 21:10:48 -0500

    Estructura del Proyecto para subir clases

---
+++ Estructura de Proyecto/Desarrollo/src/test/java/com/proyecto/Desarrollo/DesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Desarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
